e3d4aad83fedd0ae40a91e8069e6ffdb0936a612
kayesFerdous <kayesfardows@gmail.com>
2024-11-10 12:45:48 +0600

    commented migration

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,14 @@
 import { drizzle } from "drizzle-orm/postgres-js";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
+// import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
 const path = process.env.AUTH_DRIZZLE_URL as string;
 
-const migrationClient = postgres(path, {
-  max: 1,
-});
-
-migrate(drizzle(migrationClient), { migrationsFolder: "./migrations" });
+// const migrationClient = postgres(path, {
+//   max: 1,
+// });
+//
+// migrate(drizzle(migrationClient), { migrationsFolder: "./migrations" });
 
 export const queryClient = postgres(path as string);
 export const db = drizzle(queryClient);
